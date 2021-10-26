2f946ba6f02cfa02dffdb5b94e74fcdffa69326a
Micky <ad312@pm.me>
2021-10-26 23:28:54 +1100

    improvement

--- tree.py
+++ tree.py
@@ -8,18 +8,19 @@ def decisionTree():
     x_train, x_test, y_train, y_test = loadData()
     log("Running Decision Tree Classifier Model")
 
+    log("Converting 3d array into 2d")
     nTrainSamples, nTrainX, nTrainY, trainX = x_train.shape
-    x_train_2d = x_train.reshape((nTrainSamples,nTrainX*nTrainY))
+    x_train = x_train.reshape((nTrainSamples,nTrainX*nTrainY))
 
     nTestSamples, nTestX, nTestY, testX = x_test.shape
-    x_test_2d = x_test.reshape((nTestSamples,nTestX*nTestY))
+    x_test = x_test.reshape((nTestSamples,nTestX*nTestY))
 
     model = DecisionTreeClassifier(criterion = 'entropy', min_samples_split=25)
-    
-    log("Fitting model")
-    model.fit(x_train_2d, y_train)
 
-    log(f'Accuracy Score on train data: {round(accuracy_score(y_true=y_train, y_pred=model.predict(x_train_2d)),3)}')
-    log(f'Accuracy Score on the test data: {round(accuracy_score(y_true=y_test, y_pred=model.predict(x_test_2d)),3)}')
+    log("Fitting model")
+    model.fit(x_train, y_train)
+
+    log(f'Accuracy Score on train data: {round(accuracy_score(y_true=y_train, y_pred=model.predict(x_train)),3)}')
+    log(f'Accuracy Score on the test data: {round(accuracy_score(y_true=y_test, y_pred=model.predict(x_test)),3)}')
 
     return

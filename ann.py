26756d191c404995cc8d02196bd63ffccedefe5b
Micky <ad312@pm.me>
2021-11-07 13:49:00 +1100

    merge final from jaden/josh

--- ann.py
+++ ann.py
@@ -1,24 +1,25 @@
+from shared import log, loadData
 import tensorflow as tf
 import numpy as np
-import pandas as pd
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation
 from skimage import feature
 
-from shared import log, loadData
-
 # Aritificial Neural Network
 def ann():
-    x_train, x_test, y_train, y_test = loadData()
-
+    log("Running Artificial Neural Network Model")
+    x_train, x_test, y_train, y_test = loadData() # Loading in data
+    
     train_images  = np.array(x_train, dtype= 'uint8') / 255.0
     train_labels  = np.array(y_train, dtype= 'uint8')
+
     test_images = np.array(x_test, dtype= 'uint8') / 255.0
     test_labels = np.array(y_test)
 
     print(np.shape(train_images))
     print(np.shape(test_images))
-
+    
+    # Defining dense layers
     modelANN = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=[32,32]),
                                     tf.keras.layers.Dense(4096, activation=tf.nn.relu), 
                                     tf.keras.layers.Dense(4096, activation=tf.nn.relu), 
@@ -28,6 +29,7 @@ def ann():
 
     modelANN.compile(optimizer = tf.optimizers.Adam(),loss = 'sparse_categorical_crossentropy',metrics=['accuracy'])
 
+    # Defining the amount of epochs that will be calculated currently we have 10
     H=modelANN.fit(train_images, train_labels, epochs=10, validation_split=(0.1))
 
     modelANN.evaluate(test_images, test_labels)
@@ -37,7 +39,8 @@ def ann():
 # Local Binary Patterns
 def lbpann():
     log("Running Local Binary Patterns Model")
-    x_train, x_test, y_train, y_test = loadData()
+    x_train, x_test, y_train, y_test = loadData() # Loading in data
+    
     desc = LocalBinaryPatterns(24, 8)
     LBPdata_train = []
     LBPlabels_train = []
@@ -47,20 +50,21 @@ def lbpann():
     x_train = np.array(x_train[:,:,:,0])
     x_test = np.array(x_test[:,:,:,0])
 
+    # Extracting LBP features
     log("Extracting features from training dataset...")
     for img_index in range(len(x_train)):
         image = x_train[img_index]
         hist = desc.LBPfeatures(image)
-
+    
         LBPlabels_train.append(y_train[img_index])
         LBPdata_train.append(hist)
-
+    
     log("Extracted train data")
     for img_index in range(len(x_test)):
-
+        
         image = x_test[img_index]
         hist = desc.LBPfeatures(image)
-
+    
         LBPlabels_test.append(y_test[img_index])
         LBPdata_test.append(hist)
 
@@ -69,11 +73,12 @@ def lbpann():
     LBPdata_test = np.asarray(LBPdata_test)
     LBPlabels_test = np.asarray(LBPlabels_test)
 
+    
     LBPdata_train = LBPdata_train / 255.0
     LBPdata_test = LBPdata_test / 255.0
-
+    
     log("Extracted test data")
-
+    
     LBPmodel = Sequential()
 
     LBPmodel.add(Dense(512))
@@ -85,6 +90,7 @@ def lbpann():
 
     LBPmodel.add(Activation('sigmoid'))
 
+    # Inserting LBP features into ANN classifier 
     LBPmodel.compile(loss="sparse_categorical_crossentropy", optimizer=tf.keras.optimizers.Adam(0.001), metrics=['accuracy'])
     log("Compiled Model")
 
@@ -101,32 +107,33 @@ def lbpann():
 # Histogram of Oriented Gradient
 def hogann():
     log("Running Histogram of Oriented Gradients Model")
-    x_train, x_test, y_train, y_test = loadData()
+    x_train, x_test, y_train, y_test = loadData() # Loading in data
+
     log("Extracting features from training dataset...")
     HOGdata_train = []
     HOGlabels_train = []
     HOGdata_test = []
     HOGlabels_test = []
 
-    #Extracting features from train dataset
+    # Extracting features from train dataset
     for img_index in range(len(x_train)):
         image = x_train[img_index]
-
+        
         H = feature.hog(image, orientations=9, pixels_per_cell=(10, 10), cells_per_block=(2,2),
-                        transform_sqrt=True, block_norm="L2-Hys") # Complete the code 
-
+                        transform_sqrt=True, block_norm="L2-Hys")
+        
         HOGdata_train.append(H) 
         HOGlabels_train.append(y_train[img_index])
-
+    
     log("Extracted train data")
 
-    #Extracting features from test dataset
+    # Extracting features from test dataset
     for img_index in range(len(x_test)):
         image = x_test[img_index]
-
+        
         H = feature.hog(image, orientations=9, pixels_per_cell=(10, 10), cells_per_block=(2,2),
-                        transform_sqrt=True, block_norm="L2-Hys") # Complete the code 
-
+                        transform_sqrt=True, block_norm="L2-Hys")
+        
         HOGdata_test.append(H) 
         HOGlabels_test.append(y_test[img_index])
 
@@ -158,6 +165,7 @@ def hogann():
 
     return
 
+# lbp feature extraction
 class LocalBinaryPatterns:
     def __init__(self, numPoints, radius):
         self.numPoints = numPoints
@@ -172,4 +180,4 @@ class LocalBinaryPatterns:
         hist = hist.astype("float")
         hist /= (hist.sum() + eps)
 
-        return hist 
+        return hist
